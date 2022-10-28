591ba8547816d1a44a52712762c291016985e7f9
wsy <seonyoung1227@gmail.com>
2022-10-28 18:26:29 +0900

    라이브러리 설치1

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from flask import Flask
 from flask_cors import CORS
+import pymysql
+import os
 
-app: Flask = Flask(__name__)
+app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-    result = {"code": 200, "message": "hello flask"}
+    host = os.environ['DB_HOST']
+    user = os.environ['DB_USER']
+    password = os.environ['MYSQL_ROOT_PASSWORD']
+    db = os.environ['DB_NAME']
+    conn = pymysql.connect(host=host, user=user, db=db,
+                           password=password, charset='utf8')
+    curs = conn.cursor()
+    sql = "select * from student";
+    curs.execute(sql)
+    rows = curs.fetchall()
+    print(rows)
+    conn.commit()
+    conn.close()
+
+    result = {"code": 200, "message": rows}
     return result
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
